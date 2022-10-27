b44c5d7d08415bd0ae9697377f7988831e6fb525
Md Tanzil Hasan Limon <tanzilhasanlimon@gmal.com>
2022-10-28 00:36:26 +0600

    about

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
       <Route path="register" element={<Register />} />
     </Routes>
    </>
+   
   );
 
 }
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,19 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>this is about</h2>
+            <br />
+            <h2 className='text-center'>About Us</h2>
+            <br />
+            <br />
+             <div className="card text-center bg-light">
+                  <div className="card-body">
+                    <h4>Md Tanzil Hasan</h4>
+                    <p>Web Developer at Programming Hero</p>
+                    <p>Email: tanzilhasanlimon@gmail.com</p>
+                    <p>Bogura-5800 Bangladesh</p>
+
+                  </div>
+             </div>
         </div>
     );
 };
